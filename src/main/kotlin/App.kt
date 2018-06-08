67f70414aec36bf65a472abd7c8f0e7c02347c4a
kliu <kingson708@gmail.com>
2018-06-09 00:46:09 +0800

    can build executable jar to start a server

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,12 @@
+
+import spark.Request
+import spark.Response
+import spark.Spark
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("server started")
+    Spark.port(2000)
+    Spark.get("/", { request: Request, response: Response ->
+        "Hello World!"
+    })
 }
