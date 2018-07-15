b45e7b31f72714828690eb57dab5bb0c5f64a9d8
kliu <kingson708@gmail.com>
2018-07-15 22:52:20 +0800

    make nlp-input stateless

--- ui/src/app/visual-nlp/visual-nlp.component.ts
+++ ui/src/app/visual-nlp/visual-nlp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-visual-nlp',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualNLPComponent implements OnInit {
 
+  @Input()
+  nlpInput: String = '';
   constructor() { }
 
   ngOnInit() {
